e55ae199715e3365854bb345bcfb80d4fc1b05e7
Andy Smith <andy@langojango.com>
2022-01-27 15:19:52 -0800

    Added signup functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,53 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 import Helmet from "react-helmet"
 import { StaticImage } from "gatsby-plugin-image"
+import { Auth0Provider } from "@auth0/auth0-react";
+import * as queryString from "query-string";
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Helmet>
-    <script
-      type="text/javascript"
-      id="botcopy-embedder-d7lcfheammjct"
-      className="botcopy-embedder-d7lcfheammjct" 
-      data-botid="61b7d8f1f34a7e00096b54cc"
-      src = 'https://widget.botcopy.com/js/injection.js'
-      dangerouslySetInnerHTML={{
-        __html: `
-              var s = document.createElement('script'); 
-              s.type = 'text/javascript'; s.async = true; 
-              s.src = 'https://widget.botcopy.com/js/injection.js'; 
-              document.getElementById('botcopy-embedder-d7lcfheammjct').appendChild(s);
-            `,
-      }}
-    />
+const IndexPage = ({ location }) => {
+  console.log(location); // inspect location for yourself
+  const { access_token} = queryString.parse(location.hash);
+  console.log(access_token);
+  // function Botcopy() {
+  //   console.log("testing");
+  // }
+  useEffect(() => {
+    // On mount, we check if a user is logged in.
+    // If so, we'll retrieve the authenticated user's profile.
+    window.addEventListener('botcopy-events', function (e) {
+      switch(e.detail.type) {
+        case 'bc-initialized':
+          console.log("bc-initialized")
+          // do something
+          break
+  
+        case 'bc-auth-required':
+          console.log("bc-auth-required")
+          this.Botcopy.setESParameters({
+            webhookHeaders: {jwt: access_token}
+          })
+          break
+      }
+  });
+    
+  }, []);
+    return (
+        <Helmet>
+        <script
+          type="text/javascript"
+          id="botcopy-embedder-d7lcfheammjct"
+          className="botcopy-embedder-d7lcfheammjct" 
+          data-botid="61b7d8f1f34a7e00096b54cc"
+          src = 'https://widget.botcopy.com/js/injection.js'
+        >
+        </script>
+        </Helmet>
+    )
+}
 
-    </Helmet>
-
-  </Layout>
-)
 
 export default IndexPage
